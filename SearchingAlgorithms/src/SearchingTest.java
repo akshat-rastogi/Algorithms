a3d4ce31a06060dda8374aaadb2e85455a7f5d46
Akshat Rastogi <akshat.rastogi@outlook.com>
2018-11-06 14:52:33 +0000

    Linear Search added

--- SearchingAlgorithms/src/SearchingTest.java
+++ SearchingAlgorithms/src/SearchingTest.java
@@ -1,31 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+
+import algorithms.LinearSearch;
 
 public class SearchingTest {
 	
 	static int arr[];
 	static int n;
-	
+	static Scanner scan = new Scanner(System.in);
 	/***************** 
 	 * Method to get array 
 	 *****************/
 	private static void getArray(){
-		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Enter length of array:");
-		n = sc.nextInt();
-		
+		n = scan.nextInt();
 		arr = new int[n];
 		
 		System.out.println("\nEnter array elements:");
 		for (int i=0; i<n;i++){
-			arr[i] = sc.nextInt();
+			arr[i] = scan.nextInt();
 		}
-		
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try{
+			int index = -1;
+			SearchingTest.getArray();
+			
+			    	
+			System.out.println("\nEnter element to search:");
+			index = scan.nextInt();
+			
+			System.out.println(index);
+			
+			LinearSearch ls = new LinearSearch();
+			System.out.println("\n\n Element Found at index:"+ ls.linearSearch(arr,n,index)); 
+		}
+		finally{
+			if(scan!=null)
+				scan.close();
+		}
+		
 	}
 
 }
